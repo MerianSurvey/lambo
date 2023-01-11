ac4f9b4fb0644976507e283847d42ae747f7bc12
abbymintzz <abby.mintz@princeton.edu>
2023-01-11 11:27:06 -0500

    Added more function parameters

--- scripts/hsc_gaap/deploy_gaap.py
+++ scripts/hsc_gaap/deploy_gaap.py
@@ -7,25 +7,26 @@ import fire
 
 
 def deploy_training_job(tract, patch_cols, patch_rows, patch_jobs=None, filter_jobs=5,
-                        python_file='run_gaap.py',
-                        name='gaapCosmos', multijobs=False):
+                        python_file='lambo/scripts/hsc_gaap/run_gaap.py',
+                        name='gaapCosmos', multijobs=False, email="am2907@princeton.edu",
+                        repo="/projects/MERIAN/repo/", outname = "_train"):
     ''' create slurm script and then submit 
     '''
     time = "12:30:00"
     # name = name
-
+    python_file = os.path.join(os.getenv("LAMBO_HOME"), python_file)
     cntnt = '\n'.join([
         "#!/bin/bash",
-        f"#SBATCH -J %s_{tract}_{patch_rows}" % (name),
+        f"#SBATCH -J %s_{tract}_{patch_cols}_{patch_rows}" % (name),
         "#SBATCH --nodes=1",
         f"#SBATCH --ntasks-per-node={filter_jobs+1}",  # {njobs}
         f"#SBATCH --mem={int(filter_jobs * 10)}G",
         "#SBATCH --time=%s" % time,
         "#SBATCH --export=ALL",
         # f"#SBATCH --array={seed_low}-{seed_high}" if multijobs else "",
-        f"#SBATCH -o ./log/{name}_{tract}_{patch_rows}.o",
+        f"#SBATCH -o ./log/{name}_{tract}_{patch_cols}_{patch_rows}.o",
         "#SBATCH --mail-type=all",
-        "#SBATCH --mail-user=jiaxuanl@princeton.edu",
+        f"#SBATCH --mail-user={email}",
         "",
         'now=$(date +"%T")',
         'echo "start time ... $now"',
@@ -34,9 +35,9 @@ def deploy_training_job(tract, patch_cols, patch_rows, patch_jobs=None, filter_j
         'echo "Executing on the machine:" $(hostname)',
         "",
         "module purge",
-        ". /home/jiaxuanl/Research/Merian/merian_tractor/scripts/setup_env_w40.sh",
+        f". {os.getenv('LAMBO_HOME')}/lambo/scripts/setup_env_w40.sh",
         "",
-        f"python {python_file} --tract={tract} --patch_cols='{patch_cols}' --patch_rows='{patch_rows}' --patch_jobs={patch_jobs} --filter_jobs={filter_jobs} --hsc_type='S20A'",
+        f"python {python_file} --tract={tract} --patch_cols='{patch_cols}' --patch_rows='{patch_rows}' --patch_jobs={patch_jobs} --filter_jobs={filter_jobs} --hsc_type='S20A' --repo={repo}",
         "",
         "",
         'now=$(date +"%T")',
@@ -44,11 +45,11 @@ def deploy_training_job(tract, patch_cols, patch_rows, patch_jobs=None, filter_j
         ""])
 
     # create the slurm script execute it and remove it
-    f = open('_train.slurm', 'w')
+    f = open('{outname}.slurm', 'w')
     f.write(cntnt)
     f.close()
-    os.system('sbatch _train.slurm')
-    # os.system('rm _train.slurm')
+    os.system('sbatch {outname}.slurm')
+    # os.system('rm {outname}.slurm')
     return None
 
 
