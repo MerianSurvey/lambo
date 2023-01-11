59048b0ad48c9040de4baae72ddc32622722fc70
abbymintzz <abby.mintz@princeton.edu>
2023-01-11 11:27:30 -0500

    Added directory parameter

--- scripts/hsc_gaap/download_S20A.py
+++ scripts/hsc_gaap/download_S20A.py
@@ -4,9 +4,8 @@ import fire
 from unagi import hsc
 s20a = hsc.Hsc(dr='dr3', rerun='s20a_wide')
 
-
-def download_hsc_coadd(tract=9813, patch='8,6', filt='i', outdir='/projects/MERIAN/repo/S20A/deepCoadd_calexp'):
-    outdir = os.path.join(outdir, str(tract), str(patch))
+def download_hsc_coadd(tract=9813, patch='8,6', filt='i', outdir='/projects/MERIAN/repo/'):
+    outdir = os.path.join(outdir, 'S20A/deepCoadd_calexp', str(tract), str(patch))
     if not os.path.isdir(outdir):
         os.makedirs(outdir)
 
@@ -18,13 +17,15 @@ def download_hsc_coadd(tract=9813, patch='8,6', filt='i', outdir='/projects/MERI
         print('# Downloading ' + filename + ' finished! ')
 
 
-def download_blendedness(tract=9813, patch='8,6', save=False):
+def download_blendedness(tract=9813, patch='8,6', save=False, outdir='/projects/MERIAN/repo/'):
+    outdir = os.path.join(outdir, 'S20A/gaapTable')
+
     patch_x = int(patch[0])
     patch_y = int(patch[2])
     skymap_id = tract * 10000 + patch_x * 100 + patch_y
 
     sql_test = open(
-        '/home/jiaxuanl/Research/Merian/merian_tractor/scripts/hsc_gaap/blendedness.SQL', 'r').read()
+        os.path.join(os.getenv("LAMBO_HOME"), 'lambo/scripts/hsc_gaap/blendedness.SQL'), 'r').read()
     sql_test = sql_test.replace('10000000', str(skymap_id))
     print(
         f'# SQL query blendedness from HSC database for tract = {tract}, patch = {patch}')
@@ -33,28 +34,28 @@ def download_blendedness(tract=9813, patch='8,6', save=False):
         sql_test, from_file=False, preview=False, verbose=True)
 
     if save:
-        if not os.path.isdir(f'/projects/MERIAN/repo/S20A/gaapTable/{tract}/{patch}'):
+        if not os.path.isdir(f'{outdir}/{tract}/{patch}'):
             os.makedirs(
-                f'/projects/MERIAN/repo/S20A/gaapTable/{tract}/{patch}')
+                f'{outdir}/{tract}/{patch}')
         result_test.write(
-            f'/projects/MERIAN/repo/S20A/gaapTable/{tract}/{patch}/S20A_blendedness_{patch}.fits',
+            f'{outdir}/{tract}/{patch}/S20A_blendedness_{patch}.fits',
             overwrite=True)
     return result_test
 
 
-def runDownload(tract=9813):
+def runDownload(tract=9813, outdir='/projects/MERIAN/repo/'):
     from tqdm import tqdm
     import itertools
-    for i, j in tqdm(itertools.product(range(0, 8), range(0, 8))):
+    for i, j in tqdm(itertools.product(range(0, 9), range(0, 9))):
         for filt in list('grizy'):
             try:
                 download_hsc_coadd(tract=tract, patch=f'{i},{j}', filt=filt,
-                                   outdir='/projects/MERIAN/repo/S20A/deepCoadd_calexp/')
+                                   outdir=outdir)
             except Exception as e:
                 print(e)
                 print('Failed for tract = ', tract, ' patch = ',
                       f'{i},{j}', ' filter = ', filt)
-        download_blendedness(tract=tract, patch=f'{i},{j}', save=True)
+        download_blendedness(tract=tract, patch=f'{i},{j}', save=True, outdir=outdir)
 
     print('Finished for tract = ', tract)
 
