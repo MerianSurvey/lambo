dd3854fe7394a5fa65febbcb1da1a05fb1e8f2b4
abbymintzz <abby.mintz@princeton.edu>
2023-01-16 13:24:59 -0500

    Finds all merian-reduced patches automatically

--- scripts/hsc_gaap/deploy_gaap_array.py
+++ scripts/hsc_gaap/deploy_gaap_array.py
@@ -4,27 +4,45 @@ python script to deploy slurm jobs for running gaap
 import os
 import sys
 import fire
+import numpy as np
+import lsst.daf.butler as dafButler
 
-
-def deploy_training_job(tract, patch_min, patch_max, patch_jobs=None, filter_jobs=5,
+def deploy_training_job(tract, filter_jobs=5,
                         python_file='lambo/scripts/hsc_gaap/run_gaap.py',
-                        name='gaapCosmos', multijobs=False, email="am2907@princeton.edu", outname = "_train", 
-                        repo='/projects/MERIAN/repo/'):
-    ''' create slurm script and then submit 
+                        name='gaapCosmos', multijobs=False, email="am2907@princeton.edu", outname = None, 
+                        repo='/projects/MERIAN/repo/', submit=False):
+        
+    ''' Create slurm script to process all patches already reduced in Merian for a given tract.
     '''
+
+    # Get a list of reduced patches for a given tract
+
+    output_collection = "DECam/runs/merian/dr1_wide"
+    data_type = "deepCoadd_calexp"
+    skymap = "hsc_rings_v1"
+
+    butler = dafButler.Butler('/projects/MERIAN/repo/', collections=output_collection, skymap=skymap)
+    
+    patches = np.array([[data_id['tract'], data_id["patch"]] for data_id in butler.registry.queryDataIds (['tract','patch'], datasets=data_type, 
+                                                    collections=output_collection, skymap=skymap)])
+    patches = patches[patches[:, 0].argsort()]
+    del butler 
+
+    patches = np.unique(patches[patches[:,0] == tract][:,1])
+
     time = "1:30:00"
     # name = name
     python_file = os.path.join(os.getenv("LAMBO_HOME"), python_file)
     cntnt = '\n'.join([
         "#!/bin/bash",
-        f"#SBATCH -J {name}_array_{tract}_{patch_min}_{patch_max}",
+        f"#SBATCH -J {name}_array_{tract}",
         "#SBATCH --nodes=1",
         f"#SBATCH --ntasks-per-node={filter_jobs+1}",  # {njobs}
         f"#SBATCH --mem={int(filter_jobs * 10)}G",
         "#SBATCH --time=%s" % time,
-        f"#SBATCH --array={patch_min}-{patch_max}%5",
+        f"#SBATCH --array={','.join(patches.astype(str))}",
         "#SBATCH --export=ALL",
-        f"#SBATCH -o ./log/{name}_array_{tract}_%a.o",
+        f"#SBATCH -o ./log/{tract}/%a.o",
         "#SBATCH --mail-type=all",
         f"#SBATCH --mail-user={email}",
         "",
@@ -38,7 +56,7 @@ def deploy_training_job(tract, patch_min, patch_max, patch_jobs=None, filter_job
         f". {os.getenv('LAMBO_HOME')}/lambo/scripts/setup_env_w40.sh",
         f"export LAMBO_HOME='{os.getenv('LAMBO_HOME')}'",
         "",
-        f"python {python_file} --tract={tract} --patch_cols=\"[`expr $SLURM_ARRAY_TASK_ID % 9`]\" --patch_rows=\"[`expr $SLURM_ARRAY_TASK_ID / 9`]\" --patch_jobs={patch_jobs} --filter_jobs={filter_jobs} --hsc_type='S20A' --repo={repo}",
+        f"python {python_file} --tract={tract} --patch_cols=\"[`expr $SLURM_ARRAY_TASK_ID % 9`]\" --patch_rows=\"[`expr $SLURM_ARRAY_TASK_ID / 9`]\" --filter_jobs={filter_jobs} --hsc_type='S20A' --repo={repo}",
         "",
         "",
         'now=$(date +"%T")',
@@ -46,15 +64,19 @@ def deploy_training_job(tract, patch_min, patch_max, patch_jobs=None, filter_job
         ""])
 
     # create the slurm script execute it and remove it
-    f = open(f'{outname}.slurm', 'w')
+    if outname is None:
+        outname = tract
+    if not os.path.isdir('./slurmscripts'):
+        os.mkdir("./slurmscripts")
+    f = open(f'slurmscripts/{outname}.slurm', 'w')
     f.write(cntnt)
     f.close()
-    # os.system(f'sbatch {outname}.slurm')
-    # os.system('rm _train.slurm')
+    if submit:
+        os.system(f'sbatch ./slurmscripts/{outname}.slurm')
     return None
-
+    
 if __name__ == '__main__':
     fire.Fire(deploy_training_job)
 
 # Examples
-# python deploy_gaap.py --tract=9813 --patch_cols="[0,1,2,3,4,5,6,7,8]" --patch_rows="[0]" --patch_jobs=None --filter_jobs=5
+# python3 deploy_gaap_array.py --tract=8520  --repo="/scratch/gpfs/am2907/Merian/gaap" --submit=True
